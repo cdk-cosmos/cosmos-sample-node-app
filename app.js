c89615f8acd430b1abdc429b65a4a1a9a4e8d19f
Tim P <timothy@purchas.com>
2021-05-06 09:03:31 +1000

    Add Health route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/health", (req, res) => res.sendStatus(200));
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/demo", (req, res) => res.send("Hello World! from ALB(/demo)"));
 
